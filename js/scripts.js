dc412085060979fccf819b25108d6f05b0a87b34
Dan Kiss <flowfast47@gmail.com>
2019-10-24 16:31:36 -0700

    add in showSuprise DOM manipulation by adding a class

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-// alert('hi');
+function showSuprise() {
+  var suprise = document.getElementsByClassName('one')[0];
+  console.log(suprise.classList);
+  console.log(window.getComputedStyle(suprise).style);
+  suprise.classList.toggle('isVisible')
+}
